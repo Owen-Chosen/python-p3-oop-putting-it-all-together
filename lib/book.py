97db660c1180a30e17f822dbb1b67c937a82b3b3
owen <owen.chosen@gmail.com>
2023-10-02 15:10:09 +0100

    Done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    def __init__(self, title, page_count):
+        if(type(page_count) in (int,float)):
+            self.title = title
+            self.page_count = page_count
+        else:
+            self.title = title
+            print("page_count must be an integer")
+            
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    cobble = ""
+    def __init__(self, brand, size):
+        if(type(size) in (int, float)):
+            self.brand = brand
+            self.size = size
+        else:
+            self.brand = brand
+            print("size must be an integer")
+
+    def cobble(self):
+        
+
