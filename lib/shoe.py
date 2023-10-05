3e3b8d8d6c6a5a5887a23e14159caf0e45488550
owen <owen.chosen@gmail.com>
2023-10-05 04:01:57 +0100

    Done

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    cobble = ""
+
     def __init__(self, brand, size):
-        if(type(size) in (int, float)):
-            self.brand = brand
-            self.size = size
+        self.brand = brand
+        self._size = size
+
+    def get_size(self):
+        return self._size
+    
+    def set_size(self, size):
+        if type(size) in (int, float):
+            self._size = size
         else:
-            self.brand = brand
             print("size must be an integer")
 
-    def cobble(self):
-        
+    size = property(get_size, set_size)
 
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
